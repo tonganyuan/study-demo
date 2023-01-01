8b3c0c2b3e58845eea7b1295a234231e39a07fe5
tonganyuan <tonganyuan@ynby.cn>
2023-01-01 17:57:32 +0800

    init project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElmenntPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+const app = createApp(App)
+app.use(ElmenntPlus)
+app.mount('#app') 
 
 createApp(App).mount('#app')
