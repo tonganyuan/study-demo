00ba3fb6ca0836412194d2eefb3bd81a065c2be9
tonganyuan <tonganyuan@ynby.cn>
2023-01-01 22:23:59 +0800

    finish demo

---
+++ src/components/Markdown.vue
@@ -0,0 +1,14 @@
+<template>
+    <p v-html="data"></p>
+</template>
+<script>
+    import { marked } from "marked";
+    export default {
+        props:["content"],
+        computed:{
+            data(){
+                return marked(this.content);
+            }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ElmenntPlus from 'element-plus';
-import 'element-plus/lib/theme-chalk/index.css';
+import 'element-plus/dist/index.css';
 const app = createApp(App)
 app.use(ElmenntPlus)
 app.mount('#app') 
 
-createApp(App).mount('#app')
